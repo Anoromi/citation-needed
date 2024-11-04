f646998428f2006c6bc162a7a20efdb6052ae5f9
Anoromi <and.zagorulko@gmail.com>
2024-11-04 19:36:03 +0200

    init project

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="font-sans">
+    <div class="page">
+    <h1 class="font-serif">Citation needed</h1>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+
+main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.page {
+  max-width: 60rem;
+}
+
+h1 {
+  font-size: 1rem;
+  margin-top: 10vh;
+  font-size: 4rem;
+  font-weight: 400;
+  font-style: normal;
+
+}
+
+</style>
