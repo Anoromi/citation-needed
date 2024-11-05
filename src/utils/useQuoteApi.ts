99551b438c6b8ae92a49c961a65f922ff80ff73d
Anoromi <and.zagorulko@gmail.com>
2024-11-05 18:58:11 +0200

    add status to quote service

--- src/utils/useQuoteApi.ts
+++ src/utils/useQuoteApi.ts
@@ -5,35 +5,51 @@ const quotateAPI = import.meta.env.VITE_QUOTE_API;
 
 const data = ref<
   | {
-      quote?: Quote;
-      requestId: number;
-    }
+    status: "error" | "resolved" | "loading";
+    quote?: Quote;
+    error?: string;
+    requestId: number;
+  }
   | undefined
 >(undefined);
-export default function () {
+
+export default function() {
   async function findRandom() {
     const id = ++data.value!.requestId;
+    data.value!.quote = undefined;
+    data.value!.status = "loading";
     await new Promise((res) => setTimeout(res, 1000));
-    const v = await fetch(`${quotateAPI}/random`, {
-      mode: "cors",
-    });
-    const result = await v.json();
-    const quote = {
-      id: result.id,
-      tags: result.tags,
-      content: result.quote,
-      author: result.author,
-    } satisfies Quote;
-
-    if (id === data.value!.requestId) {
-      data.value!.quote = quote;
+    try {
+      const v = await fetch(`${quotateAPI}/random`, {
+        mode: "cors",
+      });
+      const result = await v.json();
+      const quote = {
+        id: result.id,
+        tags: result.tags,
+        content: result.quote,
+        author: result.author,
+      } satisfies Quote;
+      if (id === data.value!.requestId) {
+        data.value!.quote = quote;
+        data.value!.status = "resolved";
+        return quote;
+      }
+    } catch (e: any) {
+      if (id === data.value!.requestId) {
+        data.value!.status = 'error'
+        data.value!.error = e.message
+      }
     }
+
+    return null;
   }
 
   watchEffect(() => {
     if (data.value === undefined) {
       data.value = {
         requestId: 0,
+        status: "loading",
       };
       findRandom();
     }
@@ -41,6 +57,8 @@ export default function () {
 
   return {
     quote: computed(() => data.value?.quote),
+    error: computed(() => data.value?.error),
+    status: computed(() => data.value?.status),
     refresh: findRandom,
   };
 }
